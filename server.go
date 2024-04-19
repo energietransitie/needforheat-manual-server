3e44b1a35f60411f9c30b1fe26e417c9ec32377a
Labhatorian <16213031+Labhatorian@users.noreply.github.com>
2024-04-19 10:32:49 +0200

    Refactor: Rename `energyqueries` to `energy_queries`

--- server.go
+++ server.go
@@ -55,13 +55,13 @@ func NewServer(fsys fs.FS, options ServerOptions) *Server {
 	r.Handle("/devices/{device_type_name}/{manual_type_name}/{campaign_name}/*", http.FileServer(http.FS(server.fsys)))
 
 	//EnergyQuery
-	r.Handle("/energyqueries/{energy_query_type_name}/", Handler(server.handleDisplayName))
+	r.Handle("/energy_queries/{energy_query_type_name}/", Handler(server.handleDisplayName))
 
-	r.Handle("/energyqueries/{energy_query_type_name}/{manual_type_name}/", Handler(server.handleDeviceGenericRedirect))
+	r.Handle("/energy_queries/{energy_query_type_name}/{manual_type_name}/", Handler(server.handleDeviceGenericRedirect))
 
-	r.Handle("/energyqueries/{energy_query_type_name}/{manual_type_name}/{campaign_name}/", languageRedirectWithManufacturerFallback)
+	r.Handle("/energy_queries/{energy_query_type_name}/{manual_type_name}/{campaign_name}/", languageRedirectWithManufacturerFallback)
 
-	r.Handle("/energyqueries/{energy_query_type_name}/{manual_type_name}/{campaign_name}/*", http.FileServer(http.FS(server.fsys)))
+	r.Handle("/energy_queries/{energy_query_type_name}/{manual_type_name}/{campaign_name}/*", http.FileServer(http.FS(server.fsys)))
 
 	//Cloud_feeds
 	r.Handle("/cloud_feeds/{cloud_feed_type_name}/", Handler(server.handleDisplayName))
